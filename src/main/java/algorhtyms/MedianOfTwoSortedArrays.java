bfa77540622c1ce00d6e9784643d0c0a80dc1a78
Elgun Shukurov <elgunshukurov@yahoo.com>
2021-11-11 18:16:30 +0400

    Median of two sorted arrays Task has resolved. Details: runtime 3ms

---
+++ src/main/java/algorhtyms/MedianOfTwoSortedArrays.java
@@ -0,0 +1,40 @@
+package algorhtyms;
+
+import java.util.Arrays;
+
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        int[] num1 = new int[] {1,2};
+        int[] num2 = new int[] {3,4};
+
+        System.out.println(findMedianSortedArrays(num1, num2));
+    }
+
+    static double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merged = new int[nums1.length+ nums2.length];
+
+        for (int i = 0, k = 0; i < merged.length; i++,k++) {
+            if (i < nums1.length) {
+                merged[i] = nums1[k];
+            } else if (i == nums1.length) {
+                k = 0;
+                merged[i] = nums2[k];
+            } else {
+                merged[i] = nums2[k];
+            }
+        }
+
+        Arrays.sort(merged);
+        double res;
+
+        if (merged.length%2 != 0) {
+            res = merged[merged.length/2];
+        } else {
+            double fir = merged[(merged.length-1)/2];
+            double sec = merged[(merged.length+1)/2];
+            res = (fir + sec)/2;
+        }
+
+        return res;
+    }
+}
