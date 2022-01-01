a576af4e17c72b898b18052788ad2fa5fedec957
Elgun Shukurov <elgunshukurov@yahoo.com>
2022-01-01 22:39:23 +0400

    Overriding with overloading has tested.

--- src/main/java/algorhtyms/medium/AddTwoNumber.java
+++ src/main/java/algorhtyms/medium/AddTwoNumber.java
@@ -5,38 +5,78 @@ import java.util.List;
 
 public class AddTwoNumber {
     public static void main(String[] args) {
-        List<Integer> l1 = new LinkedList<>();
-        l1.add(2);
-        l1.add(4);
-        l1.add(3);
-        List<Integer> l2 = new LinkedList<>();
-        l2.add(5);
-        l2.add(6);
-        l2.add(4);
-        System.out.println(l1);
-        System.out.println(l2);
-        System.out.println(solution(l1,l2));;
+//        System.out.println(addTwoNumbers(new ListNode(23),new ListNode(45)));
     }
+//    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+//        int carry = 0;
+//        ListNode answerNode = new ListNode(0);
+//        ListNode tempNode = answerNode;
+//        while (l1 != null || l2 != null) {
+//            // If one of list is null considering null as 0, as zero will not have any impact on addition.
+//            int l1Val= (l1!=null)?l1.val:0;
+//            int l2Val= (l2!=null)?l2.val:0;
+//
+//            int currSum=l1Val+l2Val+carry;
+//            carry=currSum/10;
+//
+//            ListNode newNode=new ListNode(currSum%10);
+//            tempNode.next=newNode;
+//
+//            if(l1!=null) l1=l1.next;
+//            if(l2!=null) l2=l2.next;
+//            tempNode=tempNode.next;
+//        }
+//
+//        //If there is a left over carry
+//        if(carry>0){
+//            ListNode newNode=new ListNode(carry);
+//            tempNode.next=newNode;
+//            tempNode=tempNode.next;
+//
+//        }
+//        return answerNode.next;    }
 
-    static List<Integer> solution(List<Integer> l1, List<Integer> l2) {
-        List<Integer> l3 = new LinkedList<>();
-        int a = 0;
-        int b = 1;
-        for (Integer integer : l1) {
-            a = a + integer * b;
-            b = b * 10;
-        }
-        int c = 0;
-        int d = 1;
-        for (Integer integer : l2) {
-            c = c + integer * d;
-            d = d * 10;
-        }
-        int  e = a + c;
-        while (e>1){
-            l3.add(e%10);
-            e = e/10;
-        }
-        return l3;
-    }
+//    public static void main(String[] args) {
+//        List<Integer> l1 = new LinkedList<>();
+//        l1.add(2);
+//        l1.add(4);
+//        l1.add(3);
+//        List<Integer> l2 = new LinkedList<>();
+//        l2.add(5);
+//        l2.add(6);
+//        l2.add(4);
+//        System.out.println(l1);
+//        System.out.println(l2);
+//        System.out.println(solution(l1,l2));;
+//    }
+//
+//    static List<Integer> solution(List<Integer> l1, List<Integer> l2) {
+//        List<Integer> l3 = new LinkedList<>();
+//        int a = 0;
+//        int b = 1;
+//        for (Integer integer : l1) {
+//            a = a + integer * b;
+//            b = b * 10;
+//        }
+//        int c = 0;
+//        int d = 1;
+//        for (Integer integer : l2) {
+//            c = c + integer * d;
+//            d = d * 10;
+//        }
+//        int  e = a + c;
+//        while (e>1){
+//            l3.add(e%10);
+//            e = e/10;
+//        }
+//        return l3;
+//    }
 }
+
+// class ListNode {
+//      int val;
+//      ListNode next;
+//      ListNode() {}
+//      ListNode(int val) { this.val = val; }
+//      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+//  }
---
+++ src/main/java/algorhtyms/medium/ThreeSum.java
@@ -0,0 +1,55 @@
+package algorhtyms.medium;
+
+import java.util.*;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0};
+        System.out.println(threeSum(nums));
+    }
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+
+        if (nums.length<3) return list;
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                for (int k = j+1; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> integers = new ArrayList<>();
+                        integers.add(nums[i]);
+                        integers.add(nums[j]);
+                        integers.add(nums[k]);
+                        if (!containsDuplicate(list,integers)){
+                            list.add(integers);
+                        }
+                    }
+                }
+            }
+        }
+
+        return list;
+    }
+
+    static int count = 0;
+    public static boolean containsDuplicate(List<List<Integer>> list, List<Integer> integers) {
+        for (List<Integer> integerList : list) {
+            Collections.sort(integerList);
+        }
+        Collections.sort(integers);
+
+        for (int i = 0; i < integers.size()-1; i++) {
+            if (integers.get(i) == 0 && integers.get(i+1) ==0 && count == 0) {
+                count ++;
+                return false;
+            }
+        }
+
+        for (List<Integer> integerList : list) {
+            if (integerList.containsAll(integers)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
